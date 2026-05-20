afeb183c5e67f7d9569da7c0790802a24896e889
backstab <lukazmaj9@gmail.com>
2026-05-20 12:39:03 +0200

    old sesaon files add

---
+++ Old Season Files/S4 Rengar/DATA/Characters/Rengar/Rengar_animationTreev1.py
@@ -0,0 +1,371 @@
+=== TRACKS (4) ===
+[0] name="Channel"     idx=0  weight=1.000  mode=0x00000000
+   used by clips (3):
+      #0 "Channel"         #1 "Channel_WNDUP"   #24 "Recall"         
+
+[1] name="Actions"     idx=1  weight=1.000  mode=0x00000000
+   used by clips (15):
+      #2 "Crit"                       #4 "Death"                      
+      #10 "Spell1"                    #12 "Spell3"                    
+      #13 "Spell4"                    #14 "Spell4_Loop"               
+      #15 "Spell4_Winddown"           #16 "Spell5"                    
+      #17 "Spell6"                    #19 "Raw_LionGuy_recall"        
+      #20 "Raw_LionGuy_recall_idle"   #21 "Attack1"                   
+      #22 "Attack2"                   #23 "Attack3"                   
+      #27 "Spell1_Long"               
+
+[2] name="Spell"       idx=2  weight=1.000  mode=0x00000000
+   used by clips (1):
+      #11 "Spell2"   
+
+[3] name="Default"     idx=3  weight=1.000  mode=0x00000000
+   used by clips (10):
+      #3 "Dance"        #5 "Idle1"        #6 "Idle2"        #7 "Laugh"        #8 "Run"          
+      #9 "Run2"         #18 "Taunt"       #25 "Joke"        #26 "Idle3"       #28 "run1_Fast"   
+
+=== BLEND SUMMARY ===
+Skeleton: characters/rengar/skins/base/rengar_base.skl
+CascadeBlend: OFF
+Animations: 24
+Tracks:     4
+Clips:      29
+
+=== BLEND DATA ===
+[0] from=25 -> clips{ - }   to=21 -> clips{ run1_Fast [#28] }   flags=0x00000000   time=0.2
+[1] from=24 -> clips{ - }   to=21 -> clips{ run1_Fast [#28] }   flags=0x00000000   time=0.2
+[2] from=23 -> clips{ Taunt [#18] }   to=21 -> clips{ run1_Fast [#28] }   flags=0x00000000   time=0.2
+[3] from=22 -> clips{ Laugh [#7] }   to=21 -> clips{ run1_Fast [#28] }   flags=0x00000000   time=0.2
+[4] from=21 -> clips{ run1_Fast [#28] }   to=21 -> clips{ run1_Fast [#28] }   flags=0x00000000   time=0.2
+[5] from=20 -> clips{ Spell3 [#12] }   to=21 -> clips{ run1_Fast [#28] }   flags=0x00000000   time=0.2
+[6] from=19 -> clips{ Spell2 [#11] }   to=21 -> clips{ run1_Fast [#28] }   flags=0x00000000   time=0.2
+[7] from=18 -> clips{ Spell1_Long [#27] }   to=21 -> clips{ run1_Fast [#28] }   flags=0x00000000   time=0.2
+[8] from=17 -> clips{ Spell5 [#16] }   to=21 -> clips{ run1_Fast [#28] }   flags=0x00000000   time=0.2
+[9] from=16 -> clips{ Crit [#2] }   to=21 -> clips{ run1_Fast [#28] }   flags=0x00000000   time=0.2
+[10] from=15 -> clips{ Joke [#25] }   to=21 -> clips{ run1_Fast [#28] }   flags=0x00000000   time=0.2
+[11] from=14 -> clips{ Idle3 [#26] }   to=21 -> clips{ run1_Fast [#28] }   flags=0x00000000   time=0.2
+[12] from=13 -> clips{ Attack3 [#23] }   to=21 -> clips{ run1_Fast [#28] }   flags=0x00000000   time=0.2
+[13] from=12 -> clips{ Attack2 [#22] }   to=21 -> clips{ run1_Fast [#28] }   flags=0x00000000   time=0.2
+[14] from=11 -> clips{ Attack1 [#21] }   to=21 -> clips{ run1_Fast [#28] }   flags=0x00000000   time=0.2
+[15] from=10 -> clips{ Spell1 [#10] }   to=21 -> clips{ run1_Fast [#28] }   flags=0x00000000   time=0.2
+[16] from=9 -> clips{ Run2 [#9] }   to=21 -> clips{ run1_Fast [#28] }   flags=0x00000000   time=0.2
+[17] from=8 -> clips{ Run [#8] }   to=21 -> clips{ run1_Fast [#28] }   flags=0x00000000   time=0.2
+[18] from=7 -> clips{ Raw_LionGuy_recall_idle [#20] }   to=21 -> clips{ run1_Fast [#28] }   flags=0x00000000   time=0.2
+[19] from=6 -> clips{ Raw_LionGuy_recall [#19] }   to=21 -> clips{ run1_Fast [#28] }   flags=0x00000000   time=0.2
+[20] from=5 -> clips{ Idle2 [#6] }   to=21 -> clips{ run1_Fast [#28] }   flags=0x00000000   time=0.2
+[21] from=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   to=21 -> clips{ run1_Fast [#28] }   flags=0x00000000   time=0.2
+[22] from=3 -> clips{ Death [#4] }   to=21 -> clips{ run1_Fast [#28] }   flags=0x00000000   time=0.2
+[23] from=2 -> clips{ Dance [#3] }   to=21 -> clips{ run1_Fast [#28] }   flags=0x00000000   time=0.2
+[24] from=1 -> clips{ Channel_WNDUP [#1] }   to=21 -> clips{ run1_Fast [#28] }   flags=0x00000000   time=0.2
+[25] from=0 -> clips{ Channel [#0] }   to=21 -> clips{ run1_Fast [#28] }   flags=0x00000000   time=0.2
+[26] from=25 -> clips{ - }   to=22 -> clips{ Laugh [#7] }   flags=0x00000000   time=0.2
+[27] from=24 -> clips{ - }   to=22 -> clips{ Laugh [#7] }   flags=0x00000000   time=0.2
+[28] from=23 -> clips{ Taunt [#18] }   to=22 -> clips{ Laugh [#7] }   flags=0x00000000   time=0.2
+[29] from=22 -> clips{ Laugh [#7] }   to=22 -> clips{ Laugh [#7] }   flags=0x00000000   time=0.2
+[30] from=21 -> clips{ run1_Fast [#28] }   to=22 -> clips{ Laugh [#7] }   flags=0x00000000   time=0.2
+[31] from=20 -> clips{ Spell3 [#12] }   to=22 -> clips{ Laugh [#7] }   flags=0x00000000   time=0.2
+[32] from=19 -> clips{ Spell2 [#11] }   to=22 -> clips{ Laugh [#7] }   flags=0x00000000   time=0.2
+[33] from=18 -> clips{ Spell1_Long [#27] }   to=22 -> clips{ Laugh [#7] }   flags=0x00000000   time=0.2
+[34] from=17 -> clips{ Spell5 [#16] }   to=22 -> clips{ Laugh [#7] }   flags=0x00000000   time=0.2
+[35] from=16 -> clips{ Crit [#2] }   to=22 -> clips{ Laugh [#7] }   flags=0x00000000   time=0.2
+[36] from=15 -> clips{ Joke [#25] }   to=22 -> clips{ Laugh [#7] }   flags=0x00000000   time=0.2
+[37] from=14 -> clips{ Idle3 [#26] }   to=22 -> clips{ Laugh [#7] }   flags=0x00000000   time=0.2
+[38] from=13 -> clips{ Attack3 [#23] }   to=22 -> clips{ Laugh [#7] }   flags=0x00000000   time=0.2
+[39] from=12 -> clips{ Attack2 [#22] }   to=22 -> clips{ Laugh [#7] }   flags=0x00000000   time=0.2
+[40] from=11 -> clips{ Attack1 [#21] }   to=22 -> clips{ Laugh [#7] }   flags=0x00000000   time=0.2
+[41] from=10 -> clips{ Spell1 [#10] }   to=22 -> clips{ Laugh [#7] }   flags=0x00000000   time=0.2
+[42] from=9 -> clips{ Run2 [#9] }   to=22 -> clips{ Laugh [#7] }   flags=0x00000000   time=0.2
+[43] from=8 -> clips{ Run [#8] }   to=22 -> clips{ Laugh [#7] }   flags=0x00000000   time=0.2
+[44] from=7 -> clips{ Raw_LionGuy_recall_idle [#20] }   to=22 -> clips{ Laugh [#7] }   flags=0x00000000   time=0.2
+[45] from=6 -> clips{ Raw_LionGuy_recall [#19] }   to=22 -> clips{ Laugh [#7] }   flags=0x00000000   time=0.2
+[46] from=5 -> clips{ Idle2 [#6] }   to=22 -> clips{ Laugh [#7] }   flags=0x00000000   time=0.2
+[47] from=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   to=22 -> clips{ Laugh [#7] }   flags=0x00000000   time=0.2
+[48] from=3 -> clips{ Death [#4] }   to=22 -> clips{ Laugh [#7] }   flags=0x00000000   time=0.2
+[49] from=2 -> clips{ Dance [#3] }   to=22 -> clips{ Laugh [#7] }   flags=0x00000000   time=0.2
+[50] from=1 -> clips{ Channel_WNDUP [#1] }   to=22 -> clips{ Laugh [#7] }   flags=0x00000000   time=0.2
+[51] from=0 -> clips{ Channel [#0] }   to=22 -> clips{ Laugh [#7] }   flags=0x00000000   time=0.2
+[52] from=25 -> clips{ - }   to=23 -> clips{ Taunt [#18] }   flags=0x00000000   time=0.2
+[53] from=24 -> clips{ - }   to=23 -> clips{ Taunt [#18] }   flags=0x00000000   time=0.2
+[54] from=23 -> clips{ Taunt [#18] }   to=23 -> clips{ Taunt [#18] }   flags=0x00000000   time=0.2
+[55] from=22 -> clips{ Laugh [#7] }   to=23 -> clips{ Taunt [#18] }   flags=0x00000000   time=0.2
+[56] from=21 -> clips{ run1_Fast [#28] }   to=23 -> clips{ Taunt [#18] }   flags=0x00000000   time=0.2
+[57] from=20 -> clips{ Spell3 [#12] }   to=23 -> clips{ Taunt [#18] }   flags=0x00000000   time=0.2
+[58] from=19 -> clips{ Spell2 [#11] }   to=23 -> clips{ Taunt [#18] }   flags=0x00000000   time=0.2
+[59] from=18 -> clips{ Spell1_Long [#27] }   to=23 -> clips{ Taunt [#18] }   flags=0x00000000   time=0.2
+[60] from=17 -> clips{ Spell5 [#16] }   to=23 -> clips{ Taunt [#18] }   flags=0x00000000   time=0.2
+[61] from=16 -> clips{ Crit [#2] }   to=23 -> clips{ Taunt [#18] }   flags=0x00000000   time=0.2
+[62] from=15 -> clips{ Joke [#25] }   to=23 -> clips{ Taunt [#18] }   flags=0x00000000   time=0.2
+[63] from=14 -> clips{ Idle3 [#26] }   to=23 -> clips{ Taunt [#18] }   flags=0x00000000   time=0.2
+[64] from=13 -> clips{ Attack3 [#23] }   to=23 -> clips{ Taunt [#18] }   flags=0x00000000   time=0.2
+[65] from=12 -> clips{ Attack2 [#22] }   to=23 -> clips{ Taunt [#18] }   flags=0x00000000   time=0.2
+[66] from=11 -> clips{ Attack1 [#21] }   to=23 -> clips{ Taunt [#18] }   flags=0x00000000   time=0.2
+[67] from=10 -> clips{ Spell1 [#10] }   to=23 -> clips{ Taunt [#18] }   flags=0x00000000   time=0.2
+[68] from=9 -> clips{ Run2 [#9] }   to=23 -> clips{ Taunt [#18] }   flags=0x00000000   time=0.2
+[69] from=8 -> clips{ Run [#8] }   to=23 -> clips{ Taunt [#18] }   flags=0x00000000   time=0.2
+[70] from=7 -> clips{ Raw_LionGuy_recall_idle [#20] }   to=23 -> clips{ Taunt [#18] }   flags=0x00000000   time=0.2
+[71] from=6 -> clips{ Raw_LionGuy_recall [#19] }   to=23 -> clips{ Taunt [#18] }   flags=0x00000000   time=0.2
+[72] from=5 -> clips{ Idle2 [#6] }   to=23 -> clips{ Taunt [#18] }   flags=0x00000000   time=0.2
+[73] from=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   to=23 -> clips{ Taunt [#18] }   flags=0x00000000   time=0.2
+[74] from=3 -> clips{ Death [#4] }   to=23 -> clips{ Taunt [#18] }   flags=0x00000000   time=0.2
+[75] from=2 -> clips{ Dance [#3] }   to=23 -> clips{ Taunt [#18] }   flags=0x00000000   time=0.2
+[76] from=1 -> clips{ Channel_WNDUP [#1] }   to=23 -> clips{ Taunt [#18] }   flags=0x00000000   time=0.2
+[77] from=0 -> clips{ Channel [#0] }   to=23 -> clips{ Taunt [#18] }   flags=0x00000000   time=0.2
+[78] from=0 -> clips{ Channel [#0] }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[79] from=1 -> clips{ Channel_WNDUP [#1] }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[80] from=2 -> clips{ Dance [#3] }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[81] from=3 -> clips{ Death [#4] }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[82] from=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[83] from=5 -> clips{ Idle2 [#6] }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[84] from=6 -> clips{ Raw_LionGuy_recall [#19] }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[85] from=7 -> clips{ Raw_LionGuy_recall_idle [#20] }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[86] from=8 -> clips{ Run [#8] }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[87] from=9 -> clips{ Run2 [#9] }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[88] from=10 -> clips{ Spell1 [#10] }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[89] from=11 -> clips{ Attack1 [#21] }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[90] from=12 -> clips{ Attack2 [#22] }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[91] from=13 -> clips{ Attack3 [#23] }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[92] from=14 -> clips{ Idle3 [#26] }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[93] from=15 -> clips{ Joke [#25] }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[94] from=16 -> clips{ Crit [#2] }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[95] from=17 -> clips{ Spell5 [#16] }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[96] from=18 -> clips{ Spell1_Long [#27] }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[97] from=19 -> clips{ Spell2 [#11] }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[98] from=20 -> clips{ Spell3 [#12] }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[99] from=21 -> clips{ run1_Fast [#28] }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[100] from=22 -> clips{ Laugh [#7] }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[101] from=23 -> clips{ Taunt [#18] }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[102] from=24 -> clips{ - }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[103] from=25 -> clips{ - }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[104] from=26 -> clips{ - }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[105] from=27 -> clips{ - }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+[106] from=28 -> clips{ - }   to=4 -> clips{ Idle1 [#5], Spell4 [#13], Spell4_Loop [#14], Spell4_Winddown [#15], Spell6 [#17] }   flags=0x00000000   time=0
+
+=== CLIPS ===
+[0] Atomic "Channel"
+  ticks: 0 -> 200  tickDuration=0.0333
+  animIndex: 0
+  eventUID:  -
+  maskUID:   -
+  track:     Channel
+  syncGroup: 0
+
+[1] Atomic "Channel_WNDUP"
+  ticks: 0 -> 91  tickDuration=0.0333
+  animIndex: 1
+  eventUID:  -
+  maskUID:   -
+  track:     Channel
+  syncGroup: 0
+
+[2] Atomic "Crit"
+  ticks: 0 -> 60  tickDuration=0.0333
+  animIndex: 16
+  eventUID:  0
+  maskUID:   -
+  track:     Actions
+  syncGroup: 0
+
+[3] Atomic "Dance"
+  ticks: 0 -> 323  tickDuration=0.0333
+  animIndex: 2
+  eventUID:  1
+  maskUID:   -
+  track:     Default
+  syncGroup: 0
+
+[4] Atomic "Death"
+  ticks: 0 -> 67  tickDuration=0.0333
+  animIndex: 3
+  eventUID:  2
+  maskUID:   -
+  track:     Actions
+  syncGroup: 0
+
+[5] Atomic "Idle1"
+  ticks: 0 -> 60  tickDuration=0.0333
+  animIndex: 4
+  eventUID:  -
+  maskUID:   -
+  track:     Default
+  syncGroup: 0
+
+[6] Atomic "Idle2"
+  ticks: 0 -> 300  tickDuration=0.0333
+  animIndex: 5
+  eventUID:  -
+  maskUID:   -
+  track:     Default
+  syncGroup: 0
+
+[7] Atomic "Laugh"
+  ticks: 0 -> 118  tickDuration=0.0333
+  animIndex: 22
+  eventUID:  3
+  maskUID:   -
+  track:     Default
+  syncGroup: 0
+
+[8] Atomic "Run"
+  ticks: 0 -> 30  tickDuration=0.0333
+  animIndex: 8
+  eventUID:  -
+  maskUID:   -
+  track:     Default
+  syncGroup: 0
+
+[9] Atomic "Run2"
+  ticks: 0 -> 30  tickDuration=0.0333
+  animIndex: 9
+  eventUID:  -
+  maskUID:   -
+  track:     Default
+  syncGroup: 0
+
+[10] Atomic "Spell1"
+  ticks: 0 -> 50  tickDuration=0.0333
+  animIndex: 10
+  eventUID:  4
+  maskUID:   -
+  track:     Actions
+  syncGroup: 0
+
+[11] Atomic "Spell2"
+  ticks: 0 -> 40  tickDuration=0.0333
+  animIndex: 19
+  eventUID:  -
+  maskUID:   0
+  track:     Spell
+  syncGroup: 0
+
+[12] Atomic "Spell3"
+  ticks: 0 -> 56  tickDuration=0.0333
+  animIndex: 20
+  eventUID:  -
+  maskUID:   -
+  track:     Actions
+  syncGroup: 0
+
+[13] Atomic "Spell4"
+  ticks: 0 -> 60  tickDuration=0.0333
+  animIndex: 4
+  eventUID:  -
+  maskUID:   -
+  track:     Actions
+  syncGroup: 0
+
+[14] Atomic "Spell4_Loop"
+  ticks: 0 -> 60  tickDuration=0.0333
+  animIndex: 4
+  eventUID:  -
+  maskUID:   -
+  track:     Actions
+  syncGroup: 0
+
+[15] Atomic "Spell4_Winddown"
+  ticks: 0 -> 60  tickDuration=0.0333
+  animIndex: 4
+  eventUID:  -
+  maskUID:   -
+  track:     Actions
+  syncGroup: 0
+
+[16] Atomic "Spell5"
+  ticks: 0 -> 50  tickDuration=0.0333
+  animIndex: 17
+  eventUID:  -
+  maskUID:   -
+  track:     Actions
+  syncGroup: 0
+
+[17] Atomic "Spell6"
+  ticks: 0 -> 60  tickDuration=0.0167
+  animIndex: 4
+  eventUID:  -
+  maskUID:   -
+  track:     Actions
+  syncGroup: 0
+
+[18] Atomic "Taunt"
+  ticks: 0 -> 165  tickDuration=0.0333
+  animIndex: 23
+  eventUID:  5
+  maskUID:   -
+  track:     Default
+  syncGroup: 0
+
+[19] Atomic "Raw_LionGuy_recall"
+  ticks: 0 -> 44  tickDuration=0.0333
+  animIndex: 6
+  eventUID:  -
+  maskUID:   -
+  track:     Actions
+  syncGroup: 0
+
+[20] Atomic "Raw_LionGuy_recall_idle"
+  ticks: 0 -> 50  tickDuration=0.0333
+  animIndex: 7
+  eventUID:  -
+  maskUID:   -
+  track:     Actions
+  syncGroup: 0
+
+[21] Atomic "Attack1"
+  ticks: 0 -> 60  tickDuration=0.0333
+  animIndex: 11
+  eventUID:  6
+  maskUID:   -
+  track:     Actions
+  syncGroup: 0
+
+[22] Atomic "Attack2"
+  ticks: 0 -> 60  tickDuration=0.0333
+  animIndex: 12
+  eventUID:  7
+  maskUID:   -
+  track:     Actions
+  syncGroup: 0
+
+[23] Atomic "Attack3"
+  ticks: 0 -> 62  tickDuration=0.0333
+  animIndex: 13
+  eventUID:  8
+  maskUID:   -
+  track:     Actions
+  syncGroup: 0
+
+[24] Sequencer "Recall"
+  track: Channel
+  entries(2):
+    - clipID=19
+    - clipID=20
+
+[25] Atomic "Joke"
+  ticks: 0 -> 164  tickDuration=0.0333
+  animIndex: 15
+  eventUID:  9
+  maskUID:   -
+  track:     Default
+  syncGroup: 0
+
+[26] Atomic "Idle3"
+  ticks: 0 -> 239  tickDuration=0.0333
+  animIndex: 14
+  eventUID:  -
+  maskUID:   -
+  track:     Default
+  syncGroup: 0
+
+[27] Atomic "Spell1_Long"
+  ticks: 0 -> 50  tickDuration=0.0333
+  animIndex: 18
+  eventUID:  10
+  maskUID:   -
+  track:     Actions
+  syncGroup: 0
+
+[28] Atomic "run1_Fast"
+  ticks: 0 -> 30  tickDuration=0.0333
+  animIndex: 21
+  eventUID:  -
+  maskUID:   -
+  track:     Default
+  syncGroup: 0
